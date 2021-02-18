84a930655d851864c1baed23403f1f24e4198a36
baconlover2020 <70827873+baconlover2020@users.noreply.github.com>
2021-02-17 16:31:46 -0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import shutil
-import os
-
-pasta_categorias = "temp_categorias"
-for categoria in os.listdir(pasta_categorias):
-    shutil.unpack_archive(os.path.join(pasta_categorias,categoria), pasta_categorias)
-    os.remove(os.path.join(pasta_categorias,categoria))
